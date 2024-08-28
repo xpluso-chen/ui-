6b49495d2ff1b3ec447f2a07392c215fba18e114
xpluso-chen <xxxplusooo913@gmail.com>
2024-08-28 09:12:18 +0800

    Add files via upload

---
+++ 花網站-2/script.js
@@ -0,0 +1,58 @@
+// 移除背景影片
+$(function () {
+    if ($(window).width() < 821) {
+        $("#about video").remove();
+    }
+
+    // 抓window視窗，偵測寬度
+    // 啟用
+
+    // 交替(toggleClass)漢堡按鈕
+    $('.hamburger').click(function () {
+        // .hamburger交替加入is-active
+        $(this).toggleClass('is-active');
+        // .navigation交替加入show
+        $('.navigation').toggleClass('show');
+    });
+
+
+// 導覽列滑動到指定位置
+$('.menu a').click(function(){
+let btn = $(this).attr('href');// 取屬性和值
+let pos = $(btn).offset();// 抓相對座標位置
+$('html,body').animate({scrollTop:pos.top},1000);// 執行動畫->滾軸效果(滾軸預設在body中)
+// 1000毫秒
+
+});
+
+
+// 滑動至頂
+$('#goTop').click(function(){
+    $('html,body').animate({scrollTop:0},1000);
+    // 因為有些瀏覽器會將卷軸寫在html,有些些寫在body
+});
+
+// 至頂按鈕淡入淡出
+$(window).scroll(function(){
+    if($(this).scrollTop()>200){
+$('#goTop').stop().fadeTo('fast',1);
+    }else{
+        $('#goTop').stop().fadeOut();
+    }
+    // .stop()是讓滾軸當頁面停止時就閃一次就好，不要我上下20次就閃現閃出20次
+});
+
+
+$('.smoove').smoove({
+    offset:'40%'
+    // 滑到40%就開始出現
+});
+
+
+$('.smoove-z').smoove({
+    moveZ:'-500px',rotateX:'99deg',moveY:'250px'
+    // 從z軸-500位置出來
+});
+
+});
+
---
+++ 花網站/carousel.js
@@ -0,0 +1,13 @@
+// 自動輪播
+function bannerSwitcher() {
+    next = $('.sec-1-input').filter(':checked').next('.sec-1-input');
+    if (next.length) next.prop('checked', true);
+    else $('.sec-1-input').first().prop('checked', true);
+}
+
+var bannerTimer = setInterval(bannerSwitcher, 5000);
+
+$('nav .controls label').click(function () {
+    clearInterval(bannerTimer);
+    bannerTimer = setInterval(bannerSwitcher, 5000)
+});
