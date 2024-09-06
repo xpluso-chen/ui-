de742edbe49015d1d868351fad83e2f9569090d5
xpluso <xxxplusooo913@gmail.com>
2024-09-06 16:59:11 +0800

    banner+商品

--- 套件網站/js/scripe.js
+++ 套件網站/js/scripe.js
@@ -1,4 +1,16 @@
 $( function() {
     // 啟用JQ頁籤套件
     $( "#tabs" ).tabs();
+    $('.hover-image').hover(
+      function(){
+      // 滑鼠移入時更換圖片
+      var hoverSrc = $(this).data('hover');
+      $(this).attr('data-default', $(this).attr('src')); // 儲存原始圖片
+      $(this).attr('src', hoverSrc);
+      },
+      function(){
+      // 滑鼠移出時還原圖片
+      var hoverSrc = $(this).attr('data-default');
+      $(this).attr('src', hoverSrc);
+      });
   } );
